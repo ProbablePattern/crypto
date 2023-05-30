49e0080a48087a78cf96db69da34d1c2fd93aa36
ProbablePattern <ProbablePattern@users.noreply.github.com>
2023-05-29 23:45:12 -0400

    first

---
+++ data.py
@@ -0,0 +1,15 @@
+# First time install
+#pip3 install ccxt
+
+import ccxt
+kraken = ccxt.kraken()
+for trade in kraken.fetch_trades('BTC/USD'):
+    print(
+        f"date: {trade['datetime']} | " +
+        f"symbol: {trade['symbol']} | " +
+        f"price: {trade['price']} | " +
+        f"amount: {trade['amount']} | " +
+        f"cost: {trade['cost']} | " +
+        f"side: {trade['side']} | " +
+        f"type: {trade['type']}"
+    )
